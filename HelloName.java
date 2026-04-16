e4e93567516b2cf14b7e23eb10cdd0ef69710796
Rahmat Fauzan (2501092018) <rahmatfauzan010906@gmail.com>
2026-04-16 08:41:45 +0700

    Add files via upload

---
+++ HelloName.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_muhammad.rafli.al.fatih;
+import javax.swing.*;
+
+public class HelloName {
+    public static void main(String[] args) {
+        String name = "";
+        name = JOptionPane.showInputDialog("Please enter your name"); 
+        String msg = "Hello " + name + "!";
+        JOptionPane.showMessageDialog(null, msg);
+    }
+}
